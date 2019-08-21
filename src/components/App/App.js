4f1bc6f4e70e081b96ef09e2197e3ab64e93d5a9
Myron <dubtwee@gmail.com>
2019-08-21 18:24:22 -0500

    stretch goals incomplete

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,31 +3,69 @@ import './App.css';
 
 class App extends Component {
   state = {
-    recentRun: 0,
-    longestRun: 0,
-    milesRan: 0,
+    recentRun: {
+      distance: 0,
+      time: 0,
+      speed: 0,
+    },
+    longestRun: {
+      distance: 0,
+      time: 0,
+      speed: 0,
+    },
+    fastestRun: {
+      distance: 0,
+      time: 0,
+      speed: 0,
+    },
+    milesRan: {
+      distance: 0,
+      time: 0,
+    },
+
   };
 
   changeRun = (event, dataKey) => {
     const fieldVal = parseInt(event.target.value);
     console.log('fieldVal', fieldVal);
     this.setState({
-      milesRan: fieldVal,
-    });
+      milesRan: {
+        [dataKey]: fieldVal
+      },
+    })
   }
 
   clickAddRun = (event) => {
-    let longestRun = this.state.longestRun;
+    let longestRun = this.getLongestRun(this.state.milesRan, this.state.longestRun);
+    let fastestRun = this.state.fastestRun;
     const newRun = this.state.milesRan;
 
-    if (newRun > longestRun) {
-      longestRun = newRun;
-    }
 
     this.setState({
-      recentRun: newRun,
-      longestRun: longestRun,
-    });
+      recentRun: {
+        ...newRun,
+      },
+      longestRun: {
+        ...longestRun
+      },
+      fastestRun: {
+        ...fastestRun,
+      },
+    })
+  }
+
+  getLongestRun(currentRun, longestRun) {
+    if (currentRun.distance > longestRun.distance) {
+      return currentRun
+    }
+
+    return longestRun
+  }
+
+  speedCalculator(distance, time) {
+    const minsInHour = 60;
+    const speed = distance / (time /minsInHour);
+    return speed;
   }
 
   render() {
@@ -40,14 +78,35 @@ class App extends Component {
             <input
               type="text"
               placeholder="Distance"
-              onChange={this.changeRun}
+              onChange={(event) => this.changeRun(event, 'distance')}
+            />
+            <input
+              type="text"
+              placeholder="Time"
+              onChange={(event) => this.changeRun(event, 'time')}
             />
             <button onClick={this.clickAddRun}>Add New Run</button>
           </div>
 
           <div>
-            <p>Recent Run: {this.state.recentRun}</p>
-            <p>Longest Run: {this.state.longestRun}</p>
+            <h3>Recent Run</h3>
+            <p>distance: {this.state.recentRun.distance}</p>
+            <p>time: {this.state.recentRun.time}</p>
+            <p>Average Miles Per Hour: {this.state.recentRun.speed}</p>
+          </div>
+
+          <div>
+            <h3>Longest Run</h3>
+            <p>distance: {this.state.longestRun.distance}</p>
+            <p>time: {this.state.longestRun.time}</p>
+            <p>Average Miles Per Hour: {this.state.longestRun.speed}</p>
+          </div>
+
+          <div>
+            <h3>Fastest Run</h3>
+            <p>distance: {this.state.fastestRun.distance}</p>
+            <p>time: {this.state.fastestRun.time}</p>
+            <p>Average Miles Per Hour: {this.state.fastestRun.speed}</p>
           </div>
         </header>
       </div>
