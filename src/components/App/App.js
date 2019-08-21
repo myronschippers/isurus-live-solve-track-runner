3b115010c8adcf8698285861c36d8efb3e4abbc0
Myron <dubtwee@gmail.com>
2019-08-21 18:23:25 -0500

    completing base mode

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    recentRun: 0,
+    longestRun: 0,
+    milesRan: 0,
+  };
+
+  changeRun = (event, dataKey) => {
+    const fieldVal = parseInt(event.target.value);
+    console.log('fieldVal', fieldVal);
+    this.setState({
+      milesRan: fieldVal,
+    });
+  }
+
+  clickAddRun = (event) => {
+    let longestRun = this.state.longestRun;
+    const newRun = this.state.milesRan;
+
+    if (newRun > longestRun) {
+      longestRun = newRun;
+    }
+
+    this.setState({
+      recentRun: newRun,
+      longestRun: longestRun,
+    });
+  }
 
   render() {
     return (
@@ -10,13 +37,17 @@ class App extends Component {
           <h1>React Track Runner</h1>
 
           <div>
-            <input type="text" placeholder="Distance" />
-            <button>Add New Run</button>
+            <input
+              type="text"
+              placeholder="Distance"
+              onChange={this.changeRun}
+            />
+            <button onClick={this.clickAddRun}>Add New Run</button>
           </div>
 
           <div>
-            <p>Recent Run: 0</p>
-            <p>Longest Run: 0</p>
+            <p>Recent Run: {this.state.recentRun}</p>
+            <p>Longest Run: {this.state.longestRun}</p>
           </div>
         </header>
       </div>
