2d7f6d1ca588fe15bbbfd08ea1de5b963d24d07c
Myron <dubtwee@gmail.com>
2019-08-21 18:21:00 -0500

    adding UI elements to just have a visual show up on the site

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,16 @@ class App extends Component {
       <div>
         <header>
           <h1>React Track Runner</h1>
+
+          <div>
+            <input type="text" placeholder="Distance" />
+            <button>Add New Run</button>
+          </div>
+
+          <div>
+            <p>Recent Run: 0</p>
+            <p>Longest Run: 0</p>
+          </div>
         </header>
       </div>
     );
